60453bf8ee24073a6e6c1fd6422e2f631889ffaa
srinivas <srinivas@DESKTOP-0KM5GP1>
2022-02-02 13:17:40 +0530

    2

---
+++ Myproject/src/technoserve/c2tc/b9/Buffer.java
@@ -0,0 +1,35 @@
+package technoserve.c2tc.b9;
+import java.util.Scanner;
+
+public class Buffer {
+	
+public static void main(String args[]) 
+	{
+	Scanner sc= new Scanner(System.in);
+	String A=sc.nextLine();
+	StringBuffer sb=new StringBuffer(A);
+	for(int i=0;i<A.length();i++)
+	{
+		char ch=A.charAt(i);
+		if(ch>='A' && ch<='Z')
+		{
+			ch=(char)((int)ch+32);
+			sb.setCharAt(i,ch);
+		}
+		else
+			if(ch>='a' && ch<='z')	
+		{
+			ch=(char)((int)ch-32);
+			sb.setCharAt(i,ch);
+		}
+	}
+	    System.out.println(sb);
+	    sc.close();
+	}
+	  }
+	 
+	 
+	 
+
+
+
