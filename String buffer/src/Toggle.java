50a95dd0d5647c658594b7c085c67a3e9bfc306c
srinivas <srinivas@DESKTOP-0KM5GP1>
2022-02-02 13:31:45 +0530

    3

--- String buffer/src/Toggle.java
+++ String buffer/src/Toggle.java
@@ -2,29 +2,6 @@ import java.util.Scanner;
 
 public class Toggle 
 {
-public static void main(String args[]) 
-{
-Scanner sc= new Scanner(System.in);
-String A=sc.nextLine();
-StringBuffer sb=new StringBuffer(A);
-for(int i=0;i<A.length();i++)
-{
-	char ch=A.charAt(i);
-	if(ch>='A' && ch<='Z')
-	{
-		ch=(char)((int)ch+32);
-		sb.setCharAt(i,ch);
-	}
-	else
-		if(ch>='a' && ch<='z')	
-	{
-		ch=(char)((int)ch-32);
-		sb.setCharAt(i,ch);
-	}
-}
-    System.out.println(sb);
-    sc.close();
-}
   }
  
  
